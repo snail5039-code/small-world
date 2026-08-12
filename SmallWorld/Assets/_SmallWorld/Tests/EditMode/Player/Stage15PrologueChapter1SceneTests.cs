8c6e258021037f68c481607858cb907c35ed70ab
snail5039-code <snail5039@gmail.com>
2026-08-12 09:01:05 +0900

    test(stage15): 스토리 입력 테스트 어셈블리 경계 수정

--- SmallWorld/Assets/_SmallWorld/Tests/EditMode/Player/Stage15PrologueChapter1SceneTests.cs
+++ SmallWorld/Assets/_SmallWorld/Tests/EditMode/Player/Stage15PrologueChapter1SceneTests.cs
@@ -1,5 +1,5 @@
+using System.Reflection;
 using NUnit.Framework;
-using SmallWorld.Flow;
 using SmallWorld.Save.Stage10.Integration;
 using SmallWorld.UI.Stage7;
 using UnityEditor;
@@ -49,17 +49,17 @@ namespace SmallWorld.Player.Tests
         [Test]
         public void StoryRoute_TabOwnsAndRestoresPlayerInputAndCursor()
         {
-            StoryRouteController route = Object.FindFirstObjectByType<StoryRouteController>();
+            Component route = FindStoryRouteController();
             FirstPersonPlayerController player = Object.FindFirstObjectByType<FirstPersonPlayerController>();
 
-            Assert.That(route.HandleTabPressed(), Is.True);
-            Assert.That(route.IsRuntimeOverlayOpen, Is.True);
+            Assert.That(Invoke<bool>(route, "HandleTabPressed"), Is.True);
+            Assert.That(ReadProperty<bool>(route, "IsRuntimeOverlayOpen"), Is.True);
             Assert.That(player.enabled, Is.False);
             Assert.That(DialogueCursorMode.RequestedLockState, Is.EqualTo(CursorLockMode.None));
             Assert.That(DialogueCursorMode.RequestedVisible, Is.True);
 
-            Assert.That(route.HandleTabPressed(), Is.True);
-            Assert.That(route.IsRuntimeOverlayOpen, Is.False);
+            Assert.That(Invoke<bool>(route, "HandleTabPressed"), Is.True);
+            Assert.That(ReadProperty<bool>(route, "IsRuntimeOverlayOpen"), Is.False);
             Assert.That(player.enabled, Is.True);
             Assert.That(DialogueCursorMode.RequestedLockState, Is.EqualTo(CursorLockMode.Locked));
             Assert.That(DialogueCursorMode.RequestedVisible, Is.False);
@@ -68,16 +68,16 @@ namespace SmallWorld.Player.Tests
         [Test]
         public void StoryRoute_EscapePausesAndRestoresRuntimeState()
         {
-            StoryRouteController route = Object.FindFirstObjectByType<StoryRouteController>();
+            Component route = FindStoryRouteController();
             FirstPersonPlayerController player = Object.FindFirstObjectByType<FirstPersonPlayerController>();
 
-            Assert.That(route.HandleEscapePressed(), Is.True);
-            Assert.That(route.IsRuntimePaused, Is.True);
+            Assert.That(Invoke<bool>(route, "HandleEscapePressed"), Is.True);
+            Assert.That(ReadProperty<bool>(route, "IsRuntimePaused"), Is.True);
             Assert.That(Time.timeScale, Is.Zero);
             Assert.That(player.enabled, Is.False);
 
-            Assert.That(route.HandleEscapePressed(), Is.True);
-            Assert.That(route.IsRuntimeOverlayOpen, Is.False);
+            Assert.That(Invoke<bool>(route, "HandleEscapePressed"), Is.True);
+            Assert.That(ReadProperty<bool>(route, "IsRuntimeOverlayOpen"), Is.False);
             Assert.That(Time.timeScale, Is.EqualTo(1f));
             Assert.That(player.enabled, Is.True);
         }
@@ -85,7 +85,7 @@ namespace SmallWorld.Player.Tests
         [Test]
         public void StoryRoute_DoesNotStealInputWhileSavePanelOwnsIt()
         {
-            StoryRouteController route = Object.FindFirstObjectByType<StoryRouteController>();
+            Component route = FindStoryRouteController();
             FirstPersonPlayerController player = Object.FindFirstObjectByType<FirstPersonPlayerController>();
             var saveObject = new GameObject("Story Route Save Input Owner");
             CanvasGroup panel = saveObject.AddComponent<CanvasGroup>();
@@ -97,9 +97,9 @@ namespace SmallWorld.Player.Tests
             try
             {
                 Assert.That(savePanel.IsOpen, Is.True);
-                Assert.That(route.HandleTabPressed(), Is.False);
-                Assert.That(route.HandleEscapePressed(), Is.False);
-                Assert.That(route.IsRuntimeOverlayOpen, Is.False);
+                Assert.That(Invoke<bool>(route, "HandleTabPressed"), Is.False);
+                Assert.That(Invoke<bool>(route, "HandleEscapePressed"), Is.False);
+                Assert.That(ReadProperty<bool>(route, "IsRuntimeOverlayOpen"), Is.False);
                 Assert.That(player.enabled, Is.False);
                 Assert.That(DialogueCursorMode.RequestedLockState, Is.EqualTo(CursorLockMode.None));
             }
@@ -107,6 +107,31 @@ namespace SmallWorld.Player.Tests
             {
                 Object.DestroyImmediate(saveObject);
             }
+        }
+
+        private static Component FindStoryRouteController()
+        {
+            GameObject route = GameObject.Find("Stage 15 Story Route");
+            Assert.That(route, Is.Not.Null);
+            Component controller = route.GetComponent("StoryRouteController");
+            Assert.That(controller, Is.Not.Null);
+            return controller;
+        }
+
+        private static T Invoke<T>(Component target, string methodName)
+        {
+            MethodInfo method = target.GetType().GetMethod(methodName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            Assert.That(method, Is.Not.Null, methodName + " method is missing.");
+            return (T)method.Invoke(target, null);
+        }
+
+        private static T ReadProperty<T>(Component target, string propertyName)
+        {
+            PropertyInfo property = target.GetType().GetProperty(propertyName,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            Assert.That(property, Is.Not.Null, propertyName + " property is missing.");
+            return (T)property.GetValue(target);
         }
 
         private static void AssertNode(SerializedProperty node, string id, string displayFragment)
